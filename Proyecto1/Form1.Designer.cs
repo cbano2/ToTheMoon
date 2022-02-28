f51a32775507cf562aa6e8baa59b1ccf9aeba425
paolagamboa22 <94809434+paolagamboa22@users.noreply.github.com>
2022-02-28 15:47:59 -0500

    Paola Gamboa

---
+++ Proyecto1/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Proyecto1
+{
+    partial class Inicio
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SWAP = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.apellido = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cedulaIdentidad = new System.Windows.Forms.TextBox();
+            this.buttonSalir = new System.Windows.Forms.Button();
+            this.buttonIngresar = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Heavy", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(42, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 61);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BLINK ";
+            // 
+            // SWAP
+            // 
+            this.SWAP.AutoSize = true;
+            this.SWAP.Location = new System.Drawing.Point(397, 116);
+            this.SWAP.Name = "SWAP";
+            this.SWAP.Size = new System.Drawing.Size(0, 15);
+            this.SWAP.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Heavy", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(403, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 61);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "SWAP";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Proyecto1.Properties.Resources.ojo;
+            this.pictureBox1.Location = new System.Drawing.Point(147, -63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(311, 271);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(116, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "NOMBRE:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(108, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "APELLIDO: ";
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(220, 188);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(219, 23);
+            this.nombre.TabIndex = 6;
+            this.nombre.TextChanged += new System.EventHandler(this.nombre_TextChanged);
+            // 
+            // apellido
+            // 
+            this.apellido.Location = new System.Drawing.Point(222, 232);
+            this.apellido.Name = "apellido";
+            this.apellido.Size = new System.Drawing.Size(217, 23);
+            this.apellido.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(108, 272);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "CÉDULA I.:";
+            // 
+            // cedulaIdentidad
+            // 
+            this.cedulaIdentidad.Location = new System.Drawing.Point(223, 274);
+            this.cedulaIdentidad.Name = "cedulaIdentidad";
+            this.cedulaIdentidad.Size = new System.Drawing.Size(216, 23);
+            this.cedulaIdentidad.TabIndex = 9;
+            // 
+            // buttonSalir
+            // 
+            this.buttonSalir.Location = new System.Drawing.Point(470, 351);
+            this.buttonSalir.Name = "buttonSalir";
+            this.buttonSalir.Size = new System.Drawing.Size(75, 23);
+            this.buttonSalir.TabIndex = 11;
+            this.buttonSalir.Text = "SALIR ";
+            this.buttonSalir.UseVisualStyleBackColor = true;
+            this.buttonSalir.Click += new System.EventHandler(this.buttonSalir_Click);
+            // 
+            // buttonIngresar
+            // 
+            this.buttonIngresar.Location = new System.Drawing.Point(262, 328);
+            this.buttonIngresar.Name = "buttonIngresar";
+            this.buttonIngresar.Size = new System.Drawing.Size(75, 23);
+            this.buttonIngresar.TabIndex = 12;
+            this.buttonIngresar.Text = "INGRESAR";
+            this.buttonIngresar.UseVisualStyleBackColor = true;
+            this.buttonIngresar.Click += new System.EventHandler(this.buttonIngresar_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(605, 409);
+            this.Controls.Add(this.buttonIngresar);
+            this.Controls.Add(this.buttonSalir);
+            this.Controls.Add(this.cedulaIdentidad);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.apellido);
+            this.Controls.Add(this.nombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.SWAP);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Inicio";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Inicio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label SWAP;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private Label label4;
+        private TextBox nombre;
+        private TextBox apellido;
+        private Label label5;
+        private TextBox cedulaIdentidad;
+        private Button buttonSalir;
+        private Button buttonIngresar;
+        private ErrorProvider errorProvider1;
+    }
+}
---
+++ Proyecto1/Form2.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Proyecto1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.saludo = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // saludo
+            // 
+            this.saludo.AutoSize = true;
+            this.saludo.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.saludo.Location = new System.Drawing.Point(12, 9);
+            this.saludo.Name = "saludo";
+            this.saludo.Size = new System.Drawing.Size(193, 37);
+            this.saludo.TabIndex = 0;
+            this.saludo.Text = "BIENVENIDO, ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(469, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(130, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "papas ",
+            "coles",
+            "lechuga ",
+            "pimiento "});
+            this.comboBox1.Location = new System.Drawing.Point(209, 125);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(579, 347);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.saludo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label saludo;
+        private Button button1;
+        private Button button2;
+        private ComboBox comboBox1;
+    }
+}
