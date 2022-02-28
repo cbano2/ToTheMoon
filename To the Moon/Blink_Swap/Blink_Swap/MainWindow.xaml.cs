a2d2212366f4d0041ea5637f225f25eef2ff7d64
Carlos Baño <carlos.bano@epn.edu.ec>
2022-02-27 22:07:51 -0500

    actualizacion main
    estructuras
    ventanas

--- To the Moon/Blink_Swap/Blink_Swap/MainWindow.xaml.cs
+++ To the Moon/Blink_Swap/Blink_Swap/MainWindow.xaml.cs
@@ -18,6 +18,37 @@ namespace Blink_Swap
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    public struct Customer
+    {
+        public string address;
+        public string name;
+        public string email;
+        public string user;
+        public string password;
+
+    }
+    public struct Store
+    {
+        
+    }
+    public struct Date
+    {
+        public int day;
+        public int month;
+        public int year;
+        public Time time;
+    }
+    public struct Time
+    {
+        public int hour;
+        public int minute;
+        public int second;
+    }
+    public struct Swap
+    {
+        public Date date;
+        
+    }
     public partial class MainWindow : Window
     {
         public MainWindow()
